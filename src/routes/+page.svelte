c572df56e4a2a0d49defa2d4ff28cd73741f7fe3
Jaleel <jeremikress@gmail.com>
2024-01-26 16:45:27 +0200

    Add URL input handling and course details retrieval

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
   import URLInput from "$lib/components/URLInput.svelte";
+  import { getCourseDetails } from "$lib/util/course-details";
+  let udemyUrl: string = "";
+  let courseID: string | null= "";
+  let courseName: string | null = "";
 
   const handleSearch = (event: any) => {
-    console.log("URL from child:", event.detail.url);
+    console.log("Event from child:", event);
+    console.log("URL from child:", event.detail);
+    udemyUrl = event.detail;
     // Add your logic to handle the URL here
+  };
+
+  const obtenDetallesDelCurso = async() => {
+    console.log("Obteniendo detalles del curso...");
+    const courseDetails = await getCourseDetails(udemyUrl);
+    courseID = courseDetails['courseId'];
+    courseName = courseDetails['courseTitle'];
   };
 
   const handleClick = () => {
@@ -12,5 +25,17 @@
 </script>
 
 <div class="container">
-  <URLInput />
+  <URLInput on:urlChange={handleSearch}></URLInput>
+  {#if udemyUrl}
+   <button on:click={obtenDetallesDelCurso}
+   class="mt-4 bg-yellow-200 hover:bg-yellow-300 text-black font-bold py-2 px-4 rounded cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+   >Obtener detalles del curso</button>
+   {#if courseID}
+    <p>URL: {udemyUrl}</p>
+    <p>Course ID: {courseID}</p>
+    <p>Course Name: {courseName}</p>
+    {:else}
+    <p>Getting results...</p>
+    {/if}
+  {/if}
 </div>
