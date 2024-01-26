3f9e12b7563f5305b36d29cbdcbd6ade3e3d6c72
Jaleel <jeremikress@gmail.com>
2024-01-26 16:45:35 +0200

    Add settings page with course details display

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+
+    const { courseDetails } = data;
+
+
+    
+
+</script>
+
+<div>
+    <h1 class="text-4xl font-bold text-yellow-200 pb-2">Settings</h1>
+    <!-- <p>Course ID: {(await courseDetails).courseID}</p>
+    <p>Course Name: {(await courseDetails).courseName}</p> -->
+   {#await courseDetails}
+    <p>Getting results...</p>
+    {:then courseDetails}
+        {#if courseDetails}
+            
+            
+            
+        {:else}
+            <p>No course details found</p>
+        {/if}
+    {:catch error}
+    <p>Something went wrong: {error.message}</p>
+    {/await}
+</div>
